0aa23b6ba9a691d3ade8d39bae81e3566d45f91e
Kpena98 <82413094+Kpena98@users.noreply.github.com>
2022-03-25 15:27:51 -0300

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+projectName=$1
+csvFile=$2
+reportFile=$3
+docker run --rm -v /home/ubuntugrafico/ProyectodePruebaJmetter/PruebaJmetter:/workspace swethapn14/repo_perf:JmeterLatest -Jjmeter.save.saveservice.output_format=xml -Jcsvfile=/workspace/$csvFile -n -t /workspace/$projectName -l /workspace/$reportFile
