1cb967e362e11bda23399d179f46755c65166a59
Kpena98 <82413094+Kpena98@users.noreply.github.com>
2022-03-28 11:25:33 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,4 +2,4 @@
 projectName=$1
 csvFile=$2
 reportFile=$3
-docker run --rm -v $WORKSPACE/JmeterAutomatizado:/workspace swethapn14/repo_perf:JmeterLatest -Jjmeter.save.saveservice.output_format=xml -Jcsvfile=/workspace/$csvFile -n -t /workspace/$projectName -l /workspace/$reportFile
+docker run --rm -v $WORKSPACE:/workspace swethapn14/repo_perf:JmeterLatest -Jjmeter.save.saveservice.output_format=xml -Jcsvfile=/workspace/$csvFile -n -t /workspace/$projectName -l /workspace/$reportFile
