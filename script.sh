78d03ae9e70e745072e54c8cf9288f025c6e3514
Kpena98 <82413094+Kpena98@users.noreply.github.com>
2022-03-30 12:02:37 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-projectName=$2
 csvFile=$1
+projectName=$2
 reportFile=$3
 docker run --rm -v $WORKSPACE:/workspace swethapn14/repo_perf:JmeterLatest -Jjmeterengine.stopfail.system.exit=true -Jjmeter.save.saveservice.output_format=xml -Jcsvfile=/workspace/$csvFile -n -t /workspace/$projectName -l /workspace/$reportFile
