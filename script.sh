1fd489937526d2d6eea1eb65662180b7bb57f879
Kpena98 <82413094+Kpena98@users.noreply.github.com>
2022-03-31 17:44:46 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,3 +3,8 @@ csvFile=$1
 projectName=$2
 reportFile=$3
 docker run --rm -v $WORKSPACE:/workspace swethapn14/repo_perf:JmeterLatest -Jjmeterengine.stopfail.system.exit=true -Jjmeter.save.saveservice.output_format=xml -Jcsvfile=/workspace/$csvFile -n -t /workspace/$projectName -l /workspace/$reportFile
+if grep "false" $WORKSPACE/$reportFile
+then 
+echo El test fallo
+exit 1
+fi
