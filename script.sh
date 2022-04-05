395e9b322686a23a76167dfa8c920ea37fdc29fe
Kpena98 <82413094+Kpena98@users.noreply.github.com>
2022-04-05 15:28:19 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,8 +2,7 @@
 csvFile=$1
 projectName=$2
 reportFile=$3
-date + '%d/%m/%Y'
-varDate=$(date)
+varDate=$(date + '%d/%m/%Y')
 docker run --rm -v $WORKSPACE:/workspace swethapn14/repo_perf:JmeterLatest -Jjmeterengine.stopfail.system.exit=true -Jjmeter.save.saveservice.output_format=xml -Jcsvfile=/workspace/$csvFile -n -t /workspace/$projectName -l /workspace/$reportFile
 if grep "false" $reportFile > resultadoemail.txt && echo $JOB_NAME >> resultadoemail.txt && echo $varDate >> resultadoemail.txt
 then 
