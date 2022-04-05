d20b5990066687b15705b7893c34b6986967779a
Kpena98 <82413094+Kpena98@users.noreply.github.com>
2022-04-05 15:46:32 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,11 +5,11 @@ reportFile=$3
 varDate=$(date +%c)
 rm $reportFile
 docker run --rm -v $WORKSPACE:/workspace swethapn14/repo_perf:JmeterLatest -Jjmeterengine.stopfail.system.exit=true -Jjmeter.save.saveservice.output_format=xml -Jcsvfile=/workspace/$csvFile -n -t /workspace/$projectName -l /workspace/$reportFile
-if grep "false" $reportFile > resultadoemail.txt && echo "El Nombre del Job es:" $JOB_NAME >> resultadoemail.txt && echo "La fecha y hora de la ejecucion es:" $varDate >> resultadoemail.txt
+if grep "false" $reportFile > resultadoemail.txt && echo "El Nombre del Job es:" $JOB_NAME >> resultadoemail.txt && echo "La fecha y hora de la ejecucion fue:" $varDate >> resultadoemail.txt
 then 
 echo El test fallo
 exit 1
-else grep "true" $WORKSPACE/$reportFile > resultadoemail.txt && echo "El Nombre del Job es:" $JOB_NAME >> resultadoemail.txt && echo "La fecha y hora de la ejecucion es:" $varDate >> resultadoemail.txt 
+else grep "true" $WORKSPACE/$reportFile > resultadoemail.txt && echo "El Nombre del Job es:" $JOB_NAME >> resultadoemail.txt && echo "La fecha y hora de la ejecucion fue:" $varDate >> resultadoemail.txt 
 echo El test finalizo correctamente
 exit 0
 fi
